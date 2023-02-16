cfea6c7c9bf92b3e613c0e811be182a321867763
Brianncube <brianncubee6160@gmail.com>
2023-02-16 17:14:59 +0200

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,13 @@
 #include <stdio.h>
+/**
+ * main - prints "Programming is like building a multilingual puzzle, follwed by a newline."
+ * return 0.
+ */
 
 int main(void)
 
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
-	return (0);
+	return(0);
 }
