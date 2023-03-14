dd8e738196cfd73b14d7bd8b942a07156cfdd596
Brianncube <brianncubee6160@gmail.com>
2023-03-15 01:21:58 +0200

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,41 +1,42 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
- * str_concat - get ends of input and add together for size
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
+ * *str_concat - concatenates two strings
+ * @s1: string to concatenate
+ * @s2: other string to concatenate
+ *
+ * Return: pointer to the new string created (Success), or NULL (Error)
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *conct;
-	int i, ci;
+	int i = 0, k = 0;
+	char *combo;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	if (s2 == NULL)
 		s2 = "";
-		i = ci = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i])
 		i++;
-	while (s2[ci] != '\0')
-		ci++;
-	conct = malloc(sizeof(char) * (i + ci + 1));
-	if (conct == NULL)
-	return (NULL);
-	i = ci = 0;
-	while (s1[i] != '\0')
-	{
-		conct[i] = s1[i];
-		i++;
-	}
-	while (s2[ci] != '\0')
-	{
-		conct[i] = s2[ci];
-		i++, ci++;
-	}
-	conct[i] = '\0';
-	return (conct);
+
+	while (s2[k])
+		k++;
+
+	combo = malloc(sizeof(char) * (i + k + 1));
+
+	if (combo == NULL)
+		return (NULL);
+
+	for (i = 0; s1[i]; i++)
+		combo[i] = s1[i];
+
+	for (k = 0; s2[k]; k++, i++)
+		combo[i] = s2[k];
+
+	combo[i] = '\0';
+
+	return (combo);
 }
