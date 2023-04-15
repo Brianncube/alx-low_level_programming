446cfa6ff87d26e1c21e954dff654f91b1f6c54c
Brianncube <brianncubee6160@gmail.com>
2023-04-15 03:32:42 +0200

    finalp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -53,48 +53,4 @@ void close_file(int fd)
 * If file_to cannot be created - exit code 99.
 * If file_to or file_from cannot be closed - exit code 100.
 */
-int main(int argc, char *argv[])
-{
-	int file_from, file_to, rd, wd;
-	char *buffer;
 
-	if (argc != 3)
-	{
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
-		exit(97);
-	}
-
-	buffer = make_buffer(argv[2]);
-	file_from = open(argv[1], O_RDONLY);
-	rd = read(file_from, buffer, 1024);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
-	do {
-		if (file_from == -1 || rd == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't read from file %s\n", argv[1]);
-			free(buffer);
-			exit(98);
-		}
-
-		wd = write(file_to, buffer, rd);
-		if (file_to == -1 || wd == -1)
-		{
-			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[2]);
-			free(buffer);
-			exit(99);
-		}
-
-		rd = read(file_from, buffer, 1024);
-		file_to = open(argv[2], O_WRONLY | O_APPEND);
-
-	} while (rd > 0);
-
-	free(buffer);
-	close_file(file_from);
-	close_file(file_to);
-
-	return (0);
-}
