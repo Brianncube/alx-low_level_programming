10a34732e7f1aa2fd5066cc145fe77246ff3a734
Brianncube <brianncubee6160@gmail.com>
2023-04-15 02:22:15 +0200

    fix

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,8 @@
 */
 int create_file(const char *filename, char *text_content)
 {
-	int fd, wr, l = 0;
+	int fd, wd;
+	int l = 0;
 
 	if (filename == NULL)
 	return (-1);
@@ -17,13 +18,13 @@ int create_file(const char *filename, char *text_content)
 	if (text_content != NULL)
 	{
 	for (l = 0; text_content[l];)
-	len++;
+	l++;
 	}
 
 	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wr = write(fd, text_content, l);
+	wd = write(fd, text_content, l);
 
-	if (fd == -1 || w == -1)
+	if (fd == -1 || wd == -1)
 	return (-1);
 
 	close(fd);
